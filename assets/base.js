70707c410ae011d1eac4663d95666af1960acf23
Karan Gupta <gkaran184@gmail.com>
2020-03-14 18:23:35 +0530

    Fixed backend URLs

--- assets/base.js
+++ assets/base.js
@@ -1,7 +1,7 @@
 const environment = {
-    add_post: 'https://152.67.6.81/api/posts/new',
-    get_all_posts: 'https://152.67.6.81/api/posts/all',
-    get_post_by_id: 'https://152.67.6.81/api/posts/'
+    add_post: 'https://152.67.6.81/heisenberg/api/posts/new',
+    get_all_posts: 'https://152.67.6.81/heisenberg/api/posts/all',
+    get_post_by_id: 'https://152.67.6.81/heisenberg/api/posts/'
 }
 
 const token = 'a45c4618d6014d6711df5a8115a3168aa7bcc85bd958f5d4c4cf20fc0be16b27';
