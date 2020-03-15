b7310aafde6f582008daf3a2cafc2e3fc6f59ab0
Karan Gupta <gkaran184@gmail.com>
2020-03-15 11:13:36 +0530

    Removed old files

--- assets/base.js
+++
@@ -1,7 +0,0 @@
-const environment = {
-    add_post: 'https://karangupta.xyz/heisenberg/api/posts/new',
-    get_all_posts: 'https://karangupta.xyz/heisenberg/api/posts/all',
-    get_post_by_id: 'https://karangupta.xyz/heisenberg/api/posts/'
-}
-
-const token = 'a45c4618d6014d6711df5a8115a3168aa7bcc85bd958f5d4c4cf20fc0be16b27';
