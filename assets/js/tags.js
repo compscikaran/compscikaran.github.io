4474a591977608480c479802bc4f74c431fca4fd
Karan Gupta <gkaran184@gmail.com>
2020-05-25 19:54:06 +0530

    Fixed Layout issues
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- assets/js/tags.js
+++ assets/js/tags.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
     var url = 'https://blog.karangupta.xyz/wp-json/wp/v2/tags';
-    
+    var x = []
+
     $.ajax(url, {
         success: function (data, status, xhr) {
-            data.forEach(node => {
+            console.log(Math.round($(document).width()/100))
+            var number = Math.round($(document).width()/100);
+            data.slice(0,number/2).forEach(node => {
                 
                 var id = node["id"]
                 var name = node["name"];
