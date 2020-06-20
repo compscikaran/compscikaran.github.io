8096acaf9da75992a0d97b9c8a7ba9382d39eae5
Karan Gupta <gkaran184@gmail.com>
2020-06-20 13:02:09 +0530

    Refactored blog section to pull data from Medium
    Signed-off-by: Karan Gupta <gkaran184@gmail.com>

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,23 +1,13 @@
 $(document).ready(function() {
-    let searchParams = new URLSearchParams(window.location.search)
-    let param = searchParams.get('tag');
-    if(param != null)
-        var url = 'https://blog.karangupta.xyz/wp-json/wp/v2/posts?tags=' + param;
-    else
-        var url = 'https://blog.karangupta.xyz/wp-json/wp/v2/posts';
-    
-    $.ajax(url, {
+    $.ajax('https://api.rss2json.com/v1/api.json?rss_url=https://medium.com/feed/@compscikaran', {
         success: function(data, status, xhr) {
-            data.sort((a,b) => (a.modified > b.modified) ? -1: 1)
-            data.forEach(node => {
-                var id = node["slug"]
-                var title = node["title"]["rendered"];
-                var date = new Date(node["date"]).toISOString().split('T')[0];
-                var content = node["excerpt"]["rendered"].replace(/<[^>]*>?/gm, '');
-
-                var element = `<div class="row"><div class="col-lg-8 col-lg-offset-2 card"><a href="post.html?title=${id}"><h3>${title}</h3></a><br><p><i>${date}</i><br><br>${content}<p></div><div class="col-lg-2"></div></div><br><br>`;
-                if(node != null) {
-                    $("#content").append(element);
+            data.items.forEach(element => {
+                var title = element["title"];
+                var date = element["pubDate"];
+                var link = element["link"];
+                var content = `<a href="${link}"><h4>${title}</h4></a><p>Published: <i>${date}</i></p>`;
+                if(title != null && link != null && date != null) {
+                    $("#bloglist").append(content)
                 }
             });
         }
